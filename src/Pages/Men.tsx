92ca8710d6efa59632e4cb884461c091729cb3e7
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-10-16 12:42:56 +0200

    Change background and style for sections

--- src/Pages/Men.tsx
+++ src/Pages/Men.tsx
@@ -19,8 +19,8 @@ const images: Images[] = [
 
 const Men: FC = () => {
   return (
-    <section className="men-women-section black-background min-h-[130vh] text-limon">
-      <h2 className="pt-32 pb-16 px-8 md:px-32 md:w-4/5 lg:w-2/3 mx-auto text-center text-2xl">
+    <section className="black-background min-h-[130vh] text-limon flex flex-col items-center">
+      <h2 className="pt-32 pb-16 px-8 md:px-32 md:w-4/5 lg:w-2/3 mx-auto text-center text-base md:text-2xl">
         Dla mężczyzn proponuję rowery w eleganckich, ciemnych odcieniach.
         Rowery, które emanują siłą i charakterem, idealne dla nowoczesnych
         mężczyzn, którzy cenią sobie styl i wyraźną prezencję.
--- src/Pages/Women.tsx
+++ src/Pages/Women.tsx
@@ -19,8 +19,8 @@ const images: Images[] = [
 
 const Women: FC = () => {
   return (
-    <section className="men-women-section bg-pink-100 min-h-[130vh]">
-      <h2 className="pt-32 pb-16 px-8 md:px-32 md:w-4/5 lg:w-2/3 mx-auto text-center text-2xl text-red-900">
+    <section className="pink-background min-h-[130vh] flex flex-col items-center">
+      <h2 className="pt-32 pb-16 px-8 md:px-32 md:w-4/5 lg:w-2/3 mx-auto text-center text-base md:text-2xl text-yellow-600">
         Dla pań proponuję paletę kolorów, która jest bardziej jasna i pełna
         życia. Oferuję rowery w odcieniach, które podkreślają energię i
         indywidualność kobiet.
