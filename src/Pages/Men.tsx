1ffb772fca878bae3fb14ae234454d3db6d03548
Oskar <oskarkuchta5@gmail.com>
2023-10-31 19:45:28 +0100

    Set women section

--- src/Pages/Men.tsx
+++ src/Pages/Men.tsx
@@ -1,39 +1,16 @@
 import { FC } from "react";
-import ImageGallery from "react-image-gallery";
-import { Images } from "../Types/Gallery";
-
-const images: Images[] = [
-  {
-    original: "/2.jpg",
-    thumbnail: "/2.jpg",
-  },
-  {
-    original: "/1.jpg",
-    thumbnail: "/1.jpg",
-  },
-  {
-    original: "/3.jpg",
-    thumbnail: "/3.jpg",
-  },
-];
 
 const Men: FC = () => {
   return (
     <>
       <div className="black-background"></div>
-      <section className="min-h-[120vh] text-limon flex flex-col items-center">
+      <section className="text-limon flex flex-col items-center">
         <h2 className="pt-32 pb-16 px-8 md:px-32 md:w-4/5 lg:w-2/3 mx-auto text-center text-base md:text-2xl">
           Dla mężczyzn proponuję rowery w eleganckich, ciemnych odcieniach.
           Rowery, które emanują siłą i charakterem, idealne dla nowoczesnych
           mężczyzn, którzy cenią sobie styl i wyraźną prezencję.
         </h2>
-        <ImageGallery
-          items={images}
-          showFullscreenButton={false}
-          showNav={false}
-          additionalClass="custom-image-gallery"
-        />
-      </section>
+      </section>``
     </>
   );
 };
--- src/Pages/Women.tsx
+++ src/Pages/Women.tsx
@@ -1,36 +1,13 @@
 import { FC } from "react";
-import ImageGallery from "react-image-gallery";
-import { Images } from "../Types/Gallery";
-
-const images: Images[] = [
-  {
-    original: "/6.jpg",
-    thumbnail: "/6.jpg",
-  },
-  {
-    original: "/7.jpg",
-    thumbnail: "/7.jpg",
-  },
-  {
-    original: "/8.jpg",
-    thumbnail: "/8.jpg",
-  },
-];
 
 const Women: FC = () => {
   return (
-    <section className="pink-background min-h-[120vh] flex flex-col items-center">
-      <h2 className="pt-32 pb-16 px-8 md:px-32 md:w-4/5 lg:w-2/3 mx-auto text-center text-base md:text-2xl text-yellow-600">
+    <section className="pink-background flex flex-col items-center md:items-start">
+      <h2 className="pt-72 md:pt-64 pb-16 text-center w-3/4 md:w-1/2 text-2xl md:ml-32 md:text-4xl text-purple-900">
         Dla pań proponuję paletę kolorów, która jest bardziej jasna i pełna
         życia. Oferuję rowery w odcieniach, które podkreślają energię i
         indywidualność kobiet.
       </h2>
-      <ImageGallery
-        items={images}
-        showFullscreenButton={false}
-        showNav={false}
-        additionalClass="custom-image-gallery"
-      />
     </section>
   );
 };
