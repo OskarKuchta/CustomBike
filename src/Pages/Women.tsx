a37493361eee5aed863d536b738af5edadbad742
Oskar <oskarkuchta5@gmail.com>
2023-11-01 20:20:42 +0100

    Women section add motion div

--- src/Pages/Women.tsx
+++ src/Pages/Women.tsx
@@ -1,31 +1,61 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Link } from "react-router-dom";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import { animateScroll as scroll } from "react-scroll";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Women: FC = () => {
   const handleClick = () => {
     scroll.scrollToTop();
   };
+
+  const [inViewRef, inView] = useInView({
+    triggerOnce: true,
+    threshold: 1,
+  });
+  useEffect(() => {
+    if (inView) {
+      console.log("true");
+    } else {
+      console.log("false");
+    }
+  }, [inView]);
   return (
-    <section className="pink-background flex flex-col items-center lg:items-start">
-      <h2 className="pt-72 lg:pt-52 pb-12 text-center w-3/4 lg:w-1/2 text-base sm:text-2xl lg:ml-32 lg:text-4xl text-purple-900">
+    <section className="pink-background flex flex-col items-center lg:items-start bg-[#E9EAEB]">
+      <h2 className="pt-80 lg:pt-60 pb-12 text-center w-3/4 lg:w-1/2 text-2xl sm:text-3xl lg:ml-32 lg:text-4xl text-purple-900">
         Dla pań proponuję paletę kolorów, która jest bardziej jasna i pełna
         życia. Oferuję rowery w odcieniach, które podkreślają energię i
         indywidualność kobiet.
       </h2>
-      <img
-        src="/public/8.jpg"
-        className="mx-auto lg:ml-auto lg:mr-52 xl:mr-72 rounded-lg w-[350px] max-w-[90vw]"
-        alt="Dziewczyna na rowerze"
-      />
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0,
+        }}
+        animate={{
+          opacity: inView ? 1 : 0,
+          scale: inView ? 1 : 0,
+        }}
+        transition={{
+          duration: 2,
+        }}
+        ref={inViewRef}
+        className="mx-auto lg:ml-auto lg:mr-52 xl:mr-72 w-[350px] max-w-[90vw]"
+      >
+        <img
+          src="/public/8.jpg"
+          className="rounded-lg"
+          alt="Dziewczyna na rowerze"
+        />
+      </motion.div>
       <h2 className="mt-12 text-center mx-auto text-base sm:text-2xl w-1/2 lg:text-4xl text-purple-900">
         Jeżeli jesteś zainteresowana tym jak możemy dopasować rower do Twojego
         stylu zapraszam do kontaktu.
       </h2>
       <Link
-        className=" text-center mx-auto my-6 bg-purple-900 text-black pr-6 pl-10 py-2 rounded-md text-2xl
-             hover:text-white focus:text-white  focus:outline-none personalize-link"
+        className=" text-center mx-auto my-6 bg-purple-900 text-white	pr-6 pl-10 py-2 rounded-md text-2xl
+             hover:text-[#BEF50A] focus:text-[#BEF50A]  focus:outline-none personalize-link"
         to="/kontakt"
         onClick={handleClick}
       >
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,62 +1,6 @@
-import { useEffect, useState } from "react";
-import ImageGallery from "react-image-gallery";
-import { Images, ImageGalleryProps } from "../Types/Gallery";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Home = ({ onClick }) => {
-  const images: Images[] = [
-    {
-      original: "/1.jpg",
-    },
-    {
-      original: "/2.jpg",
-    },
-    {
-      original: "/3.jpg",
-    },
-    {
-      original: "/4.jpg",
-    },
-    {
-      original: "/5.jpg",
-    },
-    {
-      original: "/6.jpg",
-    },
-    {
-      original: "/7.jpg",
-    },
-    {
-      original: "/8.jpg",
-    },
-  ];
-  const [playGallery, setPlayGallery] = useState<boolean>(true);
-  const imageGalleryProps: ImageGalleryProps = {
-    items: images,
-    showThumbnails: false,
-    showFullscreenButton: false,
-    showPlayButton: false,
-    showBullets: false,
-    showIndex: false,
-    showNav: false,
-    autoPlay: playGallery,
-    additionalClass: "main-gallery",
-  };
-  useEffect(() => {
-    const handleHashChange = () => {
-      const hash = window.location.hash;
-      if (hash === "#home") {
-        setPlayGallery(true);
-      } else {
-        setPlayGallery(false);
-      }
-    };
-    window.addEventListener("hashchange", handleHashChange);
-    handleHashChange();
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
   return (
     <section
       id="home"
@@ -66,7 +10,6 @@ const Home = ({ onClick }) => {
         Witaj w miejscu, gdzie twoje marzenia rowerowe stają się
         rzeczywistością!
       </h1>
-      <ImageGallery {...imageGalleryProps} />
       <div
         className="double-scroll-arrows-container cursor-pointer absolute bottom-[10%] md:bottom-[5%] right-[10%]"
         onClick={onClick}
